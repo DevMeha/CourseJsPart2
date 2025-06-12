0addc3bc27062b3b43754f3893030577f3c8cda6
Meha <mixiadami@gmail.com>
2025-06-12 23:49:05 +0200

    MAKE FIRST FUNCTION

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+let hasDriversLicence = false;
+const PassTest = true;
+if (PassTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log("I can drive");
+function logger() {
+  console.log("My namew is Jonas");
+}
+// CASLLING/RUNNING /INVOKING FUNCTION
+
+logger();
