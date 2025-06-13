80e715246438b235dc9bebecc20c04a4dbff9c2c
Meha <mixiadami@gmail.com>
2025-06-13 22:45:59 +0200

    ARROW FUNCTIONS

--- script.js
+++ script.js
@@ -27,16 +27,28 @@
 
 // FUNCTION DECLARATION
 
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
-const age1 = calcAge1(1991);
-console.log(age1);
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991);
+// console.log(age1);
 
-//  FUNCTION EXPRESSION
+// //  FUNCTION EXPRESSION
 
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
+// ARROW FUNCTIONS
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
 };
-const age2 = calcAge2(1991);
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1991));
