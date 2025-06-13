815bc6cd4499d0b2164a41957caa9754c331c7f0
Meha <mixiadami@gmail.com>
2025-06-13 15:28:49 +0200

    CREATE FUNCTION DECLARATION AND FUNCTION EXPRESSION

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-"use strict";
-let hasDriversLicence = false;
-const PassTest = true;
-if (PassTest) hasDriversLicence = true;
-if (hasDriversLicence) console.log("I can drive");
-function logger() {
-  console.log("My namew is Jonas");
-}
-// CASLLING/RUNNING /INVOKING FUNCTION
+// "use strict";
+// let hasDriversLicence = false;
+// const PassTest = true;
+// if (PassTest) hasDriversLicence = true;
+// if (hasDriversLicence) console.log("I can drive");
+// function logger() {
+//   console.log("My namew is Jonas");
+// }
+// // CASLLING/RUNNING /INVOKING FUNCTION
 
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
 
-function fruitProcesor(apples, oranges) {
-  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
+// function fruitProcesor(apples, oranges) {
+//   const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+// const appleJuce = fruitProcesor(5, 0);
+// console.log(appleJuce);
+// console.log(fruitProcesor(5, 0));
+// console.log(appleJuce);
+// const appleOrangeJuice = fruitProcesor(2, 4);
+// console.log(appleOrangeJuice);
+
+// FUNCION DECLARATION VS EXPRESSIONS
+
+// FUNCTION DECLARATION
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
 }
-const appleJuce = fruitProcesor(5, 0);
-console.log(appleJuce);
-console.log(fruitProcesor(5, 0));
-console.log(appleJuce);
-const appleOrangeJuice = fruitProcesor(2, 4);
-console.log(appleOrangeJuice);
+const age1 = calcAge1(1991);
+console.log(age1);
+
+//  FUNCTION EXPRESSION
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age1, age2);
