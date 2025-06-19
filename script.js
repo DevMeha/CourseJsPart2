06d2fa97da9a87b97d06b21e21b63783dae47f81
Meha <mixiadami@gmail.com>
2025-06-19 14:12:43 +0200

    CREATE FIRST LOOP

--- script.js
+++ script.js
@@ -314,3 +314,22 @@
 // } else {
 //   console.log(`${mark.fullName} and ${john.fullName} have the same BMI (${mark.bmi.toFixed(1)})`);
 // }
+
+// LOOP
+
+// console.log("lifting weights repetition 1");
+// console.log("lifting weights repetition 2");
+// console.log("lifting weights repetition 3");
+// console.log("lifting weights repetition 4");
+// console.log("lifting weights repetition 5");
+// console.log("lifting weights repetition 6");
+// console.log("lifting weights repetition 7");
+// console.log("lifting weights repetition 8");
+// console.log("lifting weights repetition 9");
+// console.log("lifting weights repetition 10");
+
+// for loop keeps running while codition os TRUE
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
